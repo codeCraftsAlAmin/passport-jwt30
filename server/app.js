51ae7617fe6cf0c071a2445ff30f8a183f63a6ea
codeCrafts <codecraftsman12@gmail.com>
2025-01-30 14:34:55 +0600

    new changes

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: true,
+    origin: ['http://localhost:3000', 'https://passport-jwt30.vercel.app'],
     credentials: true,
   })
 );
