940ade0047380296235d9a432ae977a3a7e2f67f
codeCrafts <codecraftsman12@gmail.com>
2025-01-30 15:16:44 +0600

    new changes

--- server/app.js
+++ server/app.js
@@ -8,12 +8,7 @@ require("./config/db");
 app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: ['http://localhost:3000', 'https://passport-jwt30.vercel.app'],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // routes
 app.use(userRouter);
